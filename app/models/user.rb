95b8115c00f42b1a63cd61d1ea0e85e14cd9de22
Anton Sebvie <fatkuntroller@gmail.com>
2014-02-17 01:00:27 +0100

    Creates a micropost model and finishes the basic auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-   
+  
+  has_many :notes
+  
   def self.from_omniauth(auth)  
     find_by(provider: auth["provider"],uid: auth["uid"]) || create_with_omniauth(auth)  
   end  
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :identity,
     :on_failed_registration => IdentitiesController.action(:new)
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
 
 OmniAuth.config.on_failure = Proc.new { |env|
