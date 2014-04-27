b108fccfe0e6081d863ae1908f23854b0bb6bcb6
Anton Sebvie <anones@gamesnbass.eu>
2014-04-27 17:30:50 +0000

    Permissions added

--- app/controllers/dashboard/notes_controller.rb
+++ app/controllers/dashboard/notes_controller.rb
@@ -4,7 +4,8 @@ class Dashboard::NotesController < DashboardController
   end
   
   def show
-    @note = Note.find(params[:id])
+    if(user_permission)
+    #@note = Note.find(params[:id])
   end
   
   def new
@@ -18,23 +19,35 @@ class Dashboard::NotesController < DashboardController
   end
   
   def edit
-    @note = Note.find(params[:id])
+    if(user_permission)
   end
   
   def update
-    @note = Note.find(params[:id])
-    @note.update(note_params)
-    redirect_to dashboard_note_path(@note)
+    #@note = Note.find(params[:id])
+    if(user_permissions)
+      @note.update(note_params)
+      redirect_to dashboard_note_path(@note)
+    end
   end
   
   def destroy
-    @note = Note.find(params[:id])
-    @note.destroy
-    redirect_to dashboard_notes_path
+    #@note = Note.find(params[:id])
+    if(user_permissions)
+      @note.destroy
+      redirect_to dashboard_notes_path
+    end
   end
   
   private
-  
+    def user_permissions
+      @note = Note.find(params[:id])
+      if(@note.user_id != session[:user_id])
+        redirect_to dashboard_notes_path
+        return true;
+      else
+        return false;
+      end
+    end
     def note_params
       params.require(:note).permit(:name, :content, :user_id)
     end
